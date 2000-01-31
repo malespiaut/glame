9c6ace77c0fc7fba9b32004001d45db48af792d0
richi <richi>
2000-01-31 15:02:12 +0000

    added console glame

---
+++ src/cglame.c
@@ -0,0 +1,59 @@
+/*
+ * glame.c
+ *
+ * Copyright (C) 1999, 2000 Richard Guenther
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include "rw.h"
+#include "channel.h"
+#include "filter.h"
+#include "util.h"
+
+
+int init()
+{
+	if (hash_alloc() == -1
+	    || rw_init() == -1
+	    || filter_init() == -1
+	    || init_channel() == -1)
+		return -1;
+
+	return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+	fprintf(stderr, "\n\
+    GLAME version "VERSION", Copyright (C) 1999, 2000 Alexander Ehlert,
+    Richard Guenther, Johannes Hirche, Daniel Kobras.
+    GLAME comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions.\n\n");
+
+	if (init() == -1)
+		PANIC("Error in init!");
+
+	fprintf(stderr, "FIXME!\n");
+	exit(1);
+}
