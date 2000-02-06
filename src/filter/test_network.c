78d66d3dfe567a37b82d7aac96f3da3c8c665788
nold <nold>
2000-02-06 11:40:11 +0000

    Added new test app for filter networks. [richi]

---
+++ src/filter/test_network.c
@@ -0,0 +1,90 @@
+/*
+ * test_latency.c
+ * $Id: test_network.c,v 1.1 2000/02/06 11:40:11 nold Exp $
+ *
+ * Copyright (C) 1999, 2000 Richard Guenther
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "glame_hash.h"
+#include "filter.h"
+
+
+
+int main(int argc, char **argv)
+{
+	filter_network_t *net;
+	filter_node_t *n;
+	filter_t *f;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s network\n", argv[0]);
+		exit(1);
+	}
+
+	if (hash_alloc() == -1) {
+		fprintf(stderr, "error in initting global hash\n");
+		return -1;
+	}
+	
+	if (filter_init() == -1) {
+		fprintf(stderr, "error in filter_init()\n");
+		return -1;
+	}
+
+	if (!(f = filternetwork_load(argv[1]))) {
+		fprintf(stderr, "error in filternetwork_load(%s)\n", argv[1]);
+		return -1;
+	}
+	if (filter_add(f) == -1) {
+		fprintf(stderr, "error in filter_add()\n");
+		return -1;
+	}
+
+	if (!(net = filternetwork_new("test"))) {
+		fprintf(stderr, "error in filternetwork_new()\n");
+		return -1;
+	}
+
+	if (!(n = filternetwork_add_node(net, f->name, NULL))) {
+		fprintf(stderr, "error in filternetwork_add_node(%s)\n", f->name);
+		return -1;
+	}
+
+	fprintf(stderr, "launching network\n");
+	if (filternetwork_launch(net) == -1) {
+		fprintf(stderr, "error in filternetwork_launch()\n");
+	}
+
+	fprintf(stderr, "network launched, waiting for completion\n");
+	if (filternetwork_wait(net) == -1) {
+		fprintf(stderr, "error in filternetwork_wait()\n");
+		return -1;
+	}
+
+	fprintf(stderr, "all done.\n");
+
+	return 0;
+}
+
+
+
