424b370465e76afb0ca7d1dc8aa16db99d18e35b
garrison <garrison>
2000-02-12 21:42:30 +0000

    Initial pan filter (untested until I get a sound card working in linux)

--- src/filter/garrison.c
+++ src/filter/garrison.c
@@ -31,31 +31,90 @@ static int invert_f(filter_node_t *n)
 	filter_buffer_t *buf;
 	int i;
 
-	in = hash_find_input("input", n);	
-	out = hash_find_output("output", n);	
+	in = hash_find_input("in", n);	
+	out = hash_find_output("out", n);	
 	if (!in || !out)
 		return -1;
 
 	FILTER_AFTER_INIT;
 
-	do {
-		buf = fbuf_get(in);
-		fbuf_make_private(buf);
-		for (i = 0; i < fbuf_size(buf); i++) {
-			fbuf_buf(buf)[i] = -fbuf_buf(buf)[i];
+	while (pthread_testcancel(), buf = sbuf_get(in)) {
+		sbuf_make_private(buf);
+		for (i = 0; i < sbuf_size(buf); i++) {
+			sbuf_buf(buf)[i] = -(sbuf_buf(buf)[i]);
 		}
-		fbuf_queue(out, buf);
-	} while (pthread_testcancel(), buf);
+		sbuf_queue(out, buf);
+	}
+
+	sbuf_queue(out, buf);
 
 	FILTER_BEFORE_CLEANUP;
 
 	return 0;
 }
 
+/* This filter positions a mono sound in the stereo field */
+
 static int pan_f(filter_node_t *n)
 {
+	filter_param_t *pan;	/* between -1 and 1 */
+	filter_pipe_t *in, *l_out, *r_out;
+	filter_buffer_t *l_buf, *r_buf;
+	int i;
+
+	if (!(pan = hash_find_param("pan", n)))
+		return -1;
+	if (!(in = hash_find_input(PORTNAME_IN, n)))
+		return -1;
+	if (!(l_out = hash_find_output("left_out", n)))
+		return -1;
+	if (!(r_out = hash_find_output("right_out", n)))
+		return -1;
+
+	FILTER_AFTER_INIT;
+
+	while (pthread_testcancel(), l_buf = sbuf_get(in)) {
+		sbuf_make_private(l_buf);
+		r_buf = sbuf_alloc(sbuf_size(l_buf), n);
+		for (i = 0; i < sbuf_size(l_buf); i++) {
+			sbuf_buf(r_buf)[i] = sbuf_buf(l_buf)[i] * (pan->val.f + 1.0);
+			sbuf_buf(l_buf)[i] *= -(pan->val.f) - 1.0;
+		}
+		sbuf_queue(l_out, l_buf);
+		sbuf_queue(r_out, r_buf);
+	}
+
+	sbuf_queue(l_out, l_buf);
+	sbuf_queue(r_out, l_buf);
+
+	FILTER_BEFORE_CLEANUP;
+
+	return 0;
+}
+
+/* This filter is a complete filter for routing needs.  It has the
+   functionality of "mix" and "one2n" filters combined into one. */
+
+static int route_f(filter_node_t *n)
+{
 #if 0
-	
+	filter_buffer_t *buf;
+	filter_pipe_t *in, *out;
+
+	FILTER_AFTER_INIT;
+
+	do {
+		list_foreach_input(n, in) {
+			
+		}
+		list_foreach_output(n, out) {
+			sbuf_ref(buf);
+			sbuf_queue(out, buf);
+		}
+	} while (pthread_testcancel(), buf);
+
+	FILTER_BEFORE_CLEANUP;
+
 #endif
 	return 0;
 }
@@ -67,9 +126,9 @@ int garrison_register()
 	/***** invert filter *****/
 	if ((f = filter_alloc("Phase Inverter", "Inverses the phase of the audio signal", invert_f)) == NULL)
 		return -1;
-	if (!(filter_add_input(f, "input", "input stream to invert", FILTER_PORTTYPE_SAMPLE)))
+	if (!(filter_add_input(f, "in", "input stream to invert", FILTER_PORTTYPE_SAMPLE)))
 		return -1;
-	if (!(filter_add_output(f, "output", "inverted output stream", FILTER_PORTTYPE_SAMPLE)))
+	if (!(filter_add_output(f, "out", "inverted output stream", FILTER_PORTTYPE_SAMPLE)))
 		return -1;
 	if (filter_add(f))
 		return -1;
@@ -77,11 +136,23 @@ int garrison_register()
 	/***** pan filter *****/
 	if ((f = filter_alloc("Pan", "Positions a mono audio stream in the stereo field", pan_f)) == NULL)
 		return -1;
-	if (!(filter_add_input(f, "input", "input stream to pan", FILTER_PORTTYPE_SAMPLE)))
+	if (!(filter_add_input(f, "in", "input stream to pan", FILTER_PORTTYPE_SAMPLE)))
 		return -1;
-	if (!(filter_add_output(f, "left output", "left output stream", FILTER_PORTTYPE_SAMPLE)))
+	if (!(filter_add_output(f, "left_out", "left output stream", FILTER_PORTTYPE_SAMPLE)))
 		return -1;
-	if (!(filter_add_output(f, "right output", "right output stream", FILTER_PORTTYPE_SAMPLE)))
+	if (!(filter_add_output(f, "right_out", "right output stream", FILTER_PORTTYPE_SAMPLE)))
+		return -1;
+	if (!(filter_add_param(f, "pan", "position in stereo field", FILTER_PARAMTYPE_FLOAT)))
+		return -1;
+	if (filter_add(f))
+		return -1;
+
+	/***** route filter *****/
+	if ((f = filter_alloc("Route", "Mixes and splits audio signals", route_f)) == NULL)
+		return -1;
+	if (!(filter_add_input(f, "in", "input", FILTER_PORTTYPE_AUTOMATIC|FILTER_PORTTYPE_SAMPLE)))
+		return -1;
+	if (!(filter_add_output(f, "out", "output", FILTER_PORTTYPE_AUTOMATIC|FILTER_PORTTYPE_SAMPLE)))
 		return -1;
 	if (filter_add(f))
 		return -1;
