9d5686b7456aaccb432c383d21cc4e1d8185a252
nold <nold>
2000-02-13 12:18:42 +0000

    Cleanups... [dk]

--- src/filter/nold.c
+++ src/filter/nold.c
@@ -1,7 +1,7 @@
 /*
  * <nold.c>
  *
- * $Id: nold.c,v 1.4 2000/02/10 17:19:26 nold Exp $
+ * $Id: nold.c,v 1.5 2000/02/13 12:18:42 nold Exp $
  *
  * Copyright (C) 2000 Daniel Kobras
  *
@@ -55,9 +55,9 @@ static int sgi_audio_out_f(filter_node_t *n)
 		ssize_t		to_go;
 	} sgi_audioparam_t;
 
-	sgi_audioparam_t *in;
+	sgi_audioparam_t *in = NULL;
 	filter_pipe_t	*p_in;
-	SAMPLE		**bufs;
+	SAMPLE		**bufs = NULL;
 	SAMPLE		*zero_buf;
 	
 	int		rate;
@@ -67,11 +67,13 @@ static int sgi_audio_out_f(filter_node_t *n)
 	int		max_ch=0;
 	int		ch=0;
 
-	ALconfig	c;
-	ALport		p;
+	ALconfig	c = NULL;
+	ALport		p = NULL;
 	ALpv		v[1];
 	int		resource = AL_DEFAULT_OUTPUT;
 	int		qsize;
+
+	int		ret = -1;
 
 	/* The first run through our input pipes is basically to 
 	 * determine the number of inputs. Since we only have a
@@ -82,20 +84,20 @@ static int sgi_audio_out_f(filter_node_t *n)
 	p_in = hash_find_input("in", n);
 	if(!p_in) {
 		DPRINTF("No input channels given.\n");
-		return -1;
+		goto _cleanup;
 	}
 	rate = filterpipe_sample_rate(p_in);
 	do {
 		max_ch++;
 		if(rate != filterpipe_sample_rate(p_in)) {
 			DPRINTF("Sample rate mismatch!\n");
-			return -1;
+			goto _cleanup;
 		}
 	} while((p_in = hash_next_input(p_in)));
 
 	if(rate <= 0) {
 		DPRINTF("No valid sample rate given.\n");
-		return -1;
+		goto _cleanup;
 	}
 
 	/* 'in' is our internal array of pipe goodies. 'bufs' was added
@@ -105,9 +107,8 @@ static int sgi_audio_out_f(filter_node_t *n)
 	in = (sgi_audioparam_t *)malloc(max_ch * sizeof(sgi_audioparam_t));
 	bufs = (SAMPLE **)malloc(max_ch * sizeof(SAMPLE *));
 	if(!in || !bufs) {
-		free(in);
 		DPRINTF("Failed to alloc input structs.\n");
-		return -1;
+		goto _cleanup;
 	}
 
 	/* Second pass through the input pipes to set up our internal
@@ -123,16 +124,14 @@ static int sgi_audio_out_f(filter_node_t *n)
 	} while((p_in = hash_next_input(p_in)));
 	
 	/* Paranoia. */
-	if(ch != max_ch) {
-		DPRINTF("Huh!? Input pipes changed under us!?\n");
-		return -1;
-	}
+	if(ch != max_ch) 
+		PANIC("Huh!? Input pipes changed under us!?\n");
 			
 	c = alNewConfig();
 	if(!c) {
 		DPRINTF("Failed to create audio configuration: %s\n",
 			alGetErrorString(oserror()));
-			return -1;
+			goto _cleanup;
 	}
 
 	/* SGI AL has this nice feature of directly supporting our
@@ -147,18 +146,18 @@ static int sgi_audio_out_f(filter_node_t *n)
 	if(alSetDevice(c, resource) < 0) {
 		DPRINTF("Resource invalid: %s\n",
 			alGetErrorString(oserror()));
-		return -1;
+		goto _cleanup;
 	}
 	v[0].param = AL_RATE;
 	v[0].value.ll = alDoubleToFixed(rate);
 	if(alSetParams(resource, v, 1) < 0) {
 		DPRINTF("Failed to set audio output parameters: %s\n",
 			alGetErrorString(oserror()));
-		return -1;
+		goto _cleanup;
 	}
 	if(v[0].sizeOut < 0) {
 		DPRINTF("Invalid sample rate.\n");
-		return -1;
+		goto _cleanup;
 	}
 	/* The QueueSized is used as an initializer to our output chunk
 	 * size later on.
@@ -166,21 +165,23 @@ static int sgi_audio_out_f(filter_node_t *n)
 	qsize = alGetQueueSize(c);
 	if(qsize <= 0) {
 		DPRINTF("Invalid QueueSize.\n");
-		return -1;
+		goto _cleanup;
 	}
 	p = alOpenPort("GLAME audio output", "w", c);
 	if(!p) {
 		DPRINTF("Failed to open audio output port: %s\n",
 			alGetErrorString(oserror()));
-			return -1;
+			goto _cleanup;
 	}
 	alFreeConfig(c);
+	c = NULL;
 
 	/* Uh-uh! Playing dirty games here! Wish me luck... */
 	zero_buf = (SAMPLE *)calloc(qsize, sizeof(SAMPLE));
 	
 	FILTER_AFTER_INIT;
 	/* May not fail from now on... */
+	ret = 0;
 	
 	ch = 0;
 	ch_active = max_ch;
@@ -191,7 +192,7 @@ static int sgi_audio_out_f(filter_node_t *n)
 	 */
 	goto _entry;
 	
-	while(ch_active) {
+	while(pthread_testcancel(), ch_active) {
 
 #if 0
 		DPRINTF("Writing %d sample chunk.\n", chunk_size);
@@ -233,11 +234,15 @@ static int sgi_audio_out_f(filter_node_t *n)
 
 	FILTER_BEFORE_CLEANUP;
 
-	alClosePort(p);
+_cleanup:
+	if(p)
+		alClosePort(p);
+	if(c)
+		alFreeConfig(c);
+	/* free() is supposed to handle NULLs gracefully. */
 	free(bufs);
 	free(in);
-	DPRINTF("LEAVING\n");
-	return 0;
+	return ret;
 }
 
 #endif
