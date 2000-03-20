de2077a6d72d2f9ffd4f4fb32e3ab61a5087f2f0
mag <mag>
2000-03-20 12:42:39 +0000

    added chebyshev filter coefficient calculation

--- src/plugins/maggy.c
+++ src/plugins/maggy.c
@@ -1,6 +1,6 @@
 /*
  * maggy.c
- * $Id: maggy.c,v 1.4 2000/03/20 10:04:01 richi Exp $
+ * $Id: maggy.c,v 1.5 2000/03/20 12:42:39 mag Exp $
  *
  * Copyright (C) 2000 Alexander Ehlert
  *
@@ -261,11 +261,156 @@ static int debugrms_f(filter_node_t *n){
 	FILTER_RETURN;
 }
 			
+/* chebyshev calculates coefficients for a chebyshev filter
+ * a,b coefficients
+ * n   number of poles(2,4,6,...)
+ * m   0..lowpass, 1..highpass
+ * fc  cutoff frequency in percent of samplerate
+ * pr  percent ripple in passband (0.5 is optimal)
+ *
+ * Code from DSPGUIDE Chapter 20, pp341
+ */
+
+#define GLAME_IIR_LOWPASS  0
+#define GLAME_IIR_HIGHPASS 1
+
+int chebyshev(float *a, float *b, int n, int mode, float fc, float pr){
+
+	int i,j,res=-1;
+	float *ta,*tb;
+	float *aa,*bb;
+	float sa,sb;
+	float gain;
+	float rp,ip,es,vx,kx,t,w,m,d,k;
+	float *x,*y;
+	float h;
+	
+	if (mode)
+	  DPRINTF("\nHighpass mode\n");
+	else
+	  DPRINTF("\nLowpass mode\n");
+	
+	if ((n%2)!=0)
+		goto _error;
+	if (!(ta=ALLOCN(n+2,float)))
+		goto _error;
+	if (!(tb=ALLOCN(n+2,float)))
+		goto _error;
+	aa=ALLOCN(3,float);
+	bb=ALLOCN(2,float);
+	x=ALLOCN(3,float);
+	y=ALLOCN(2,float);
+
+	if ((fc<0) || (fc>0.5)) 
+		goto _error;
+	a[2]=1.0;
+	b[2]=1.0;
+
+	for(i=0;i<n/2;i++){
+		h=M_PI/(n*2.0)+i*M_PI/n;
+		rp=-cos(h);
+		ip=sin(h);
+/*	
+		DPRINTF("\nrp=%f\nip=%f\n",rp,ip);
+*/
+		if(pr>0.0) {
+			h=100.0/(100.0-pr);
+			es=sqrt(h*h-1.0);
+			h=1.0/es;
+			vx=1.0/n*log(h+sqrt(h*h+1));
+			kx=1.0/n*log(h+sqrt(h*h-1));
+			kx=(exp(kx)+exp(-kx))/2.0;
+			h=exp(vx);
+			rp*=(h-1.0/h)*0.5/kx;
+			ip*=(h+1.0/h)*0.5/kx;
+		}
+
+		t=2.0*tan(0.5);
+		w=2.0*M_PI*fc;
+		m=rp*rp+ip*ip;
+		d=4.0-4.0*rp*t+m*t*t;
+		x[0]=t*t/d;
+		x[1]=2*x[0];
+		x[2]=x[0];
+		y[0]=(8.0-2.0*m*t*t)/d;
+		y[1]=(-4.0-4.0*rp*t-m*t*t)/d;
+/*
+		DPRINTF("\nt=%f\nw=%f\nm=%f\nd=%f\n",t,w,m,d);
+		DPRINTF("\nx0=%f\nx1=%f\nx2=%f\ny1=%f\ny2=%f\n",x[0],x[1],x[2],y[0],y[1]);
+*/
+		if (mode)
+			k=-cos(w*0.5+0.5)/cos(w*0.5-0.5);
+		else
+			k=sin(0.5-w*0.5)/sin(0.5+w*0.5);
+		d=1+y[0]*k-y[1]*k*k;
+		aa[0]=(x[0]-x[1]*k+x[2]*k*k)/d;
+		aa[1]=(-2.0*x[0]*k+x[1]+x[1]*k*k-2.0*x[2]*k)/d;
+		aa[2]=(x[0]*k*k-x[1]*k+x[2])/d;
+		bb[0]=(2.0*k+y[0]+y[0]*k*k-2.0*y[1]*k)/d;
+		bb[1]=(-k*k-y[0]*k+y[1])/d;
+		if(mode){
+			aa[1]=-aa[1];
+			bb[1]=-bb[1];
+		}
+/*			
+		DPRINTF("\na0=%f\na1=%f\na2=%f\nb0=%f\nb1=%f\n",aa[0],aa[1],aa[2],bb[0],bb[1]);
+*/
+		memcpy(ta,a,sizeof(float)*n);
+		memcpy(tb,b,sizeof(float)*n);
+		for(j=2;j<n+2;j++){
+			a[j]=aa[0]*ta[j]+aa[1]*ta[j-1]+aa[2]*ta[j-2];
+			b[j]=tb[j]-bb[0]*tb[j-1]-bb[1]*tb[j-2];
+		}
+	}
+	
+	b[2]=0.0;
+	for(i=0;i<n+1;i++){
+		a[i]=a[i+2];
+		b[i]=b[i+2];
+	}
+	sa=sb=0.0;
+	for(i=0;i<n+1;i++){
+		if (!mode){
+			sa+=a[i];
+			sb+=b[i];
+		}else{
+			sa+=a[i]*pow(-1.0,i);
+			sb+=b[i]*pow(-1.0,i);
+		}
+	}
+	
+	gain=sa/(1.0-sb);
+	for(i=0;i<n+1;i++)
+		a[i]/=gain;
+	
+	res=0;
+_error:
+	free(aa);
+	free(bb);
+	free(ta);
+	free(tb);
+	free(x);
+	free(y);
+	return res;
+}
+	
+void debugcheb(){
+	float *a,*b;
+
+	a=ALLOCN(8,float);
+	b=ALLOCN(8,float);
+	chebyshev(a,b,4,GLAME_IIR_LOWPASS,0.1,0);
+	chebyshev(a,b,4,GLAME_IIR_HIGHPASS,0.1,0);
+	free(a);
+	free(b);
+}
+
 int maggy_register()
 {
 	filter_t *f;
 	filter_paramdesc_t *p;
-
+	
+	debugcheb();
 	if (!(f = filter_alloc(noisegate_f))
 	    || !filter_add_input(f, PORTNAME_IN, "input",
 				 FILTER_PORTTYPE_SAMPLE)
