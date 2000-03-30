a520cf87d0437c8e5f9ccb94fe2735530d45b17f
mag <mag>
2000-03-30 12:00:10 +0000

    lots of bugfixes to iir, needs still a few more

--- src/plugins/maggy.c
+++ src/plugins/maggy.c
@@ -1,6 +1,6 @@
 /*
  * maggy.c
- * $Id: maggy.c,v 1.9 2000/03/30 09:20:30 mag Exp $
+ * $Id: maggy.c,v 1.10 2000/03/30 12:00:10 mag Exp $
  *
  * Copyright (C) 2000 Alexander Ehlert
  *
@@ -58,31 +58,36 @@ struct glame_iir {
 	int nstages;	/* Number of filterstages */
 	int na;		/* number of a coefficients per stage */
 	int nb;		/* number of b coefficients per stage */
-	float fc;	/* cutoff frequency */
-	float ppr;	/* percent of ripple in passband */
-	float spr;	/* percent of ripple in stopand */
-	float **coeff;	/* Actual filter coefficients */
+	gliirt fc;	/* cutoff frequency */
+	gliirt ppr;	/* percent of ripple in passband */
+	gliirt spr;	/* percent of ripple in stopand */
+	gliirt **coeff;	/* Actual filter coefficients */
 };
 
 glame_iir_t *init_glame_iir(int mode, int nstages, int na, int nb){
 	glame_iir_t *dum=NULL;
-
+	int i;
+	DPRINTF("Trying to allocate glame_iir_t\n");
 	if ((dum=ALLOCN(1,glame_iir_t))){
+		DPRINTF("glame_iir_t success\n");
 		dum->mode=mode;
 		dum->nstages=nstages;
 		dum->na=na;
 		dum->nb=nb;
-		dum->coeff=(float **)malloc(nstages*(na+nb)*sizeof(float));
-		if (!dum->coeff){
-			free(dum);
-			return NULL;
-		}
+		dum->coeff=(gliirt **)malloc(nstages*sizeof(gliirt *));
+		for(i=0;i<nstages;i++)
+			dum->coeff[i]=(gliirt *)malloc((na+nb)*sizeof(gliirt));
 	}
 	return dum;
 }
 
 void free_glame_iir(glame_iir_t *gt){
+	int i;
+	DPRINTF("free coeff");
+	for(i=0;i<gt->nstages;i++)
+		free(gt->coeff[i]);
 	free(gt->coeff);
+	DPRINTF("free gt");
 	free(gt);
 }
 
@@ -98,41 +103,46 @@ void free_glame_iir(glame_iir_t *gt){
  * online version http://www.dspguide.com
  */
 
-int chebyshev_stage(glame_iir_t *gt, int n, int np){
-	float h,rp,ip,es,kx,vx,t,w,m,d,k,sum;
-	float *x,*y,*a,*b;
-	int res=-1;
+#define chebtype float
+
+int chebyshev_stage(glame_iir_t *gt, int n){
+	chebtype h,rp,ip,es,kx,vx,t,w,m,d,k,sum;
+	chebtype *x,*y,*a,*b;
+	int res=-1,i;
 	
 	if (n>gt->nstages)
 		goto _error;
 	if (gt->na+gt->nb!=5)
 		goto _error;
 
-	if (!(x=ALLOCN(3,float)))
+	if (!(x=ALLOCN(3,chebtype)))
 		goto _error;
-	if (!(y=ALLOCN(2,float)))
+	if (!(y=ALLOCN(2,chebtype)))
 		goto _error;
-	if (!(a=ALLOCN(3,float)))
+	if (!(a=ALLOCN(3,chebtype)))
 		goto _error;
-	if (!(b=ALLOCN(2,float)))
+	if (!(b=ALLOCN(2,chebtype)))
 		goto _error;
 	
-	h=M_PI/(gt->np*2.0)+n*M_PI/gt->np;
+	h=M_PI/((chebtype)gt->np*2.0)+n*M_PI/(chebtype)gt->np;
 	rp=-cos(h);
 	ip=sin(h);
+	/*
+	DPRINTF("rp=%f ip=%f np=%d h=%f ppr=%f\n",rp,ip,gt->np,h,gt->ppr);
+	*/
 	if(gt->ppr>0.0) {
 		h=100.0/(100.0-gt->ppr);
-		es=sqrtf(h*h-1.0);
+		es=sqrt(h*h-1.0);
 		h=1.0/es;
-		vx=1.0/n*logf(h+sqrtf(h*h+1.0));
-		kx=1.0/n*logf(h+sqrtf(h*h-1.0));
-		kx=(expf(kx)+expf(-kx))/2.0;
-		h=expf(vx);
+		vx=1.0/(chebtype)gt->np*log(h+sqrt(h*h+1.0));
+		kx=1.0/(chebtype)gt->np*log(h+sqrt(h*h-1.0));
+		kx=(exp(kx)+exp(-kx))/2.0;
+		h=exp(vx);
 		rp*=(h-1.0/h)*0.5/kx;
 		ip*=(h+1.0/h)*0.5/kx;
 	}
 /*
-		DPRINTF("\ni=%d rp=%f ip=%f es=%f kx=%f vx=%f\n",i,rp,ip,es,kx,vx);
+	DPRINTF("rp=%f ip=%f es=%f kx=%f vx=%f h=%f\n",rp,ip,es,kx,vx,h);
 */
 	t=2.0*tan(0.5);
 	w=2.0*M_PI*gt->fc;
@@ -144,8 +154,8 @@ int chebyshev_stage(glame_iir_t *gt, int n, int np){
 	y[0]=(8.0-2.0*m*t*t)/d;
 	y[1]=(-4.0-4.0*rp*t-m*t*t)/d;
 /*
-		DPRINTF("\nt=%f\nw=%f\nm=%f\nd=%f\n",t,w,m,d);
-		DPRINTF("\nx0=%f\nx1=%f\nx2=%f\ny1=%f\ny2=%f\n",x[0],x[1],x[2],y[0],y[1]);
+	DPRINTF("\nt=%f\nw=%f\nm=%f\nd=%f\n",t,w,m,d);
+	DPRINTF("\nx0=%f\nx1=%f\nx2=%f\ny1=%f\ny2=%f\n",x[0],x[1],x[2],y[0],y[1]);
 */
 	if (gt->mode)
 		k=-cos(w*0.5+0.5)/cos(w*0.5-0.5);
@@ -164,12 +174,20 @@ int chebyshev_stage(glame_iir_t *gt, int n, int np){
 	/* Adjust gain for single stages
 	 * FIXME Don't know if that works
 	 */
-	sum=a[0]+fabs(a[1])+a[2];
-	gt->coeff[n][0]=a[0]/sum;
-	gt->coeff[n][1]=a[1]/sum;
-	gt->coeff[n][2]=a[2]/sum;
-	gt->coeff[n][3]=b[0];
-	gt->coeff[n][4]=b[1];
+	/*
+	sum=a[0]+abs(a[1])+a[2];
+	DPRINTF("a0=%f a1=%f a2=%f\n",a[0],a[1],a[2]);
+	for(i=0;i<3;i++) a[i]/=sum;
+*/
+/*
+	DPRINTF("n=%d a0=%f a1=%f a2=%f b1=%f b2=%f sum=%f\n",n,a[0],a[1],a[2],b[0],b[1],sum);
+*/	
+	gt->coeff[n][0]=(gliirt)(a[0]);
+	gt->coeff[n][1]=(gliirt)(a[1]);
+	gt->coeff[n][2]=(gliirt)(a[2]);
+	gt->coeff[n][3]=(gliirt)(b[0]);
+	gt->coeff[n][4]=(gliirt)(b[1]);
+	
 	res=0;
 _error:
 	free(x);
@@ -198,11 +216,14 @@ glame_iir_t *chebyshev(int n, int mode, float fc, float pr){
 		return NULL;
 	}
 
-	if (!(gt=init_glame_iir(mode,n/2,3,2))){
+	if ((gt=init_glame_iir(mode,n/2,3,2))){
+		DPRINTF("init_glame_iir done!\n");
 		gt->ppr=pr;
 		gt->fc=fc;
+		gt->np=n;
 		for(i=0;i<n/2;i++)
-			chebyshev_stage(gt,i,n);
+			chebyshev_stage(gt,i);
+		DPRINTF("Filter setup done!\n");
 	}
 	return gt;	
 }
@@ -222,7 +243,7 @@ static int iir_f(filter_node_t *n)
 	glame_iir_t *gt;
 	iirf_t *iirf;
 	SAMPLE *s;
-	int i,pos,j,nb,nt;
+	int i,pos,j,nb,nt,z;
 
 	if (!(in = filternode_get_input(n, PORTNAME_IN))
 	    || !(out = filternode_get_output(n, PORTNAME_OUT)))
@@ -235,7 +256,8 @@ static int iir_f(filter_node_t *n)
 	 * Then you can register all kind of filters that are using iir_f as kernel
 	 */
 	
-	gt=chebyshev(4,GLAME_IIR_LOWPASS,0.1,0.5);
+	if (!(gt=chebyshev(4,GLAME_IIR_LOWPASS,0.2,0.5)))
+		FILTER_ERROR_RETURN("chebyshev failed");
 	
 	/* here follow generic code */
 	
@@ -246,6 +268,13 @@ static int iir_f(filter_node_t *n)
 		FILTER_ERROR_RETURN("memory allocation error");
 
 	for(i=0;i<gt->nstages;i++){
+		DPRINTF("Stage %d\n",i);
+		for(j=0;j<gt->na;j++)
+			DPRINTF("a[%d]=%f ",j,gt->coeff[i][j]);
+		DPRINTF("\n");
+		for(;j<gt->na+gt->nb;j++)
+			DPRINTF("b[%d]=%f ",j-gt->nb,gt->coeff[i][j]);
+		DPRINTF("\n");
 		if (!(iirf[i].iring=(gliirt*)malloc(gt->na*sizeof(gliirt))))
 			FILTER_ERROR_RETURN("memory allocation error");
 		if (!(iirf[i].oring=(gliirt*)malloc((gt->nb+1)*sizeof(gliirt))))
@@ -269,17 +298,29 @@ static int iir_f(filter_node_t *n)
 			for(i=0;i<gt->nstages;i++){
 				iirf[i].oring[iirf[i].opos]=0.0;
 				/* y[n]=a0*x[n]+a1*x[n-1]+... */
-				for(j=0;j<gt->na;j++)
-				  iirf[i].oring[iirf[i].opos]+=gt->coeff[i][j]*iirf[i].iring[(iirf[i].ipos-j)%gt->na];
+				for(j=0;j<gt->na;j++){
+					z=iirf[i].ipos-j;
+					if (z<0)
+						z=gt->na-z;
+					else if (z>gt->na)
+						z=z-gt->na;
+					iirf[i].oring[iirf[i].opos]+=gt->coeff[i][j]*iirf[i].iring[z];
+				}
 				/* y[n]=y[n]+b1*y[n-1]+b2*y[n-2]+... */
-				for(j=gt->na;j<nt;j++)
-				  iirf[i].oring[iirf[i].opos]+=gt->coeff[i][j]*iirf[i].oring[(iirf[i].opos-j-1)%nb];
+				for(j=gt->na;j<nt;j++){
+					z=iirf[i].opos-j-1;
+					if (z<0)
+						z=gt->nb-z;
+					else if (z>gt->nb)
+						z=z-gt->nb;
+				  	iirf[i].oring[iirf[i].opos]+=gt->coeff[i][j]*iirf[i].oring[z];
+				}
 				/* Feed output of last stage to next stage */
 				if (i+1<gt->nstages)
 					iirf[i+1].iring[iirf[i+1].ipos]=iirf[i].oring[iirf[i].opos];
 			}
 			/* At least I process it in place :) */
-			sbuf_buf(inb)[pos++]=iirf[i].oring[iirf[i].opos];
+			sbuf_buf(inb)[pos++]=iirf[gt->nstages-1].oring[iirf[gt->nstages-1].opos];
 			/* Adjust ringbuffers */
 			for(i=0;i<gt->nstages;i++){
 				iirf[i].ipos++;
