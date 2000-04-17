1d16aa223cf4d308647ba82c2faa4c18174b9c8e
richi <richi>
2000-04-17 09:47:40 +0000

    - added wavetest to the Makefile - added some swapfile code to wavetest.c

--- src/gui/wavetest.c
+++ src/gui/wavetest.c
@@ -11,6 +11,65 @@
 #include <time.h>
 #include <gtk/gtk.h>
 #include "gtkwavedraw.h"
+#include "swapfile.h"
+
+
+/* Simple and very stupid wave callback using the
+ * swapfile API (just file 0). For more intelligent
+ * usage we might need more "clever" handling of
+ * the callback result. A different callback might
+ * have the following prototype:
+ *   struct gliovec *callback(void *handle,
+ *                            glong from, glong to, gint step);
+ * where struct gliovec is similar to the readv/writev one:
+ *   struct gliovec {
+ *       gfloat *data;
+ *       glong from;
+ *       glong to;
+ *       gint step;
+ *   };
+ *
+ * The gtk_wave_draw_add_by_callback() would take an additional
+ * void *handle argument which is passed along to the callback.
+ */
+
+static fileid_t fid = 1; /* HACK */
+static gfloat *wave = NULL; /* HACK, too */
+
+/* umm, I dont understand semantics of step...
+ */
+static gfloat *swapfile_callback(gint wave_idx,
+				 glong from, glong to, gint step)
+{
+	gfloat *s, *w;
+	filecluster_t *fc;
+
+	printf("callback with from %li to %li step %i\n", from, to, step);
+	if (wave)
+		free(wave);
+	wave = (gfloat *)malloc(sizeof(gfloat)*(to-from+step-1)/step);
+
+	w = wave;
+	while (from <= to) {
+		if (!(fc = filecluster_get(fid, from*sizeof(gfloat)))) {
+			printf("no filecluster at %li\n", from);
+			return NULL;
+		}
+		s = (gfloat *)filecluster_mmap(fc);
+		while (from <= filecluster_end(fc)/sizeof(gfloat)
+		       && from <= to) {
+			*w++ = *s;
+			s += step;
+			from += step;
+		}
+		filecluster_munmap(fc);
+	}
+
+	return wave;
+}
+
+
+
 
 static gfloat *
 generate_random_data(int n_vals)
@@ -48,7 +107,7 @@ int main(int argc, char **argv)
   gint npoints;
 
   gtk_init(&argc, &argv);
-  
+
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
   gtk_window_set_title(GTK_WINDOW(window), "Test Wave Widget");
@@ -70,19 +129,30 @@ int main(int argc, char **argv)
   wavedraw = gtk_wave_draw_new();
   gtk_widget_show(wavedraw);
 
-  /* Some basic starting stuff */
-  gtk_wave_draw_zoom(GTK_WAVE_DRAW(wavedraw), 0, npoints);
-  gtk_wave_draw_set_resolution(GTK_WAVE_DRAW(wavedraw),10);
+  if (argc > 1 && swap_open(argv[1], 0) == 0) {
+	  /* Some basic starting stuff */
+	  gtk_wave_draw_zoom(GTK_WAVE_DRAW(wavedraw), 0, npoints);
+	  gtk_wave_draw_set_resolution(GTK_WAVE_DRAW(wavedraw), 10);
+
+	  /* Add a swapfile wave - HACK */
+	  gtk_wave_draw_add_wave_by_call(GTK_WAVE_DRAW(wavedraw),
+					 swapfile_callback,
+					 file_size(fid)/sizeof(gfloat), 0);
+  } else {
+	  /* Some basic starting stuff */
+	  gtk_wave_draw_zoom(GTK_WAVE_DRAW(wavedraw), 0, npoints);
+	  gtk_wave_draw_set_resolution(GTK_WAVE_DRAW(wavedraw),10);
+
+	  /* Add a wave */
+	  fltary = generate_random_data(npoints);
+	  gtk_wave_draw_add_wave(GTK_WAVE_DRAW(wavedraw), fltary, npoints, 0);
   
-  /* Add a wave */
-  fltary = generate_random_data(npoints);
-  gtk_wave_draw_add_wave(GTK_WAVE_DRAW(wavedraw), fltary, npoints, 0);
+	  /* Add another wave */
+	  fltary = generate_random_data(npoints);
+	  gtk_wave_draw_add_wave(GTK_WAVE_DRAW(wavedraw), fltary, npoints, 0);
   
-  /* Add another wave */
-  fltary = generate_random_data(npoints);
-  gtk_wave_draw_add_wave(GTK_WAVE_DRAW(wavedraw), fltary, npoints, 0);
-  
-  g_free(fltary);
+	  g_free(fltary);
+  }
 
   /* Draw the widget, this populates the backbuffer */
   gtk_widget_draw(wavedraw, NULL);
