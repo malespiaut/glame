ab3e50bf9bf978c6778b99f9ab3e9e0898d54daa
richi <richi>
2000-09-25 10:05:56 +0000

    work around an autoconf/libtool/automake bug (autoconf 2.49a only)

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-rm -f config.cache
+rm -f config.cache libtool ltconfig ltmain.sh
 aclocal -I . -I macros
 libtoolize --copy --automake
-#--ltdl
 autoheader
 autoconf
+touch ltconfig
 automake --add-missing
