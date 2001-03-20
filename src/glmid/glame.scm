2a5d2aaac4363c66ae3b4491c7ca7d8f6accbeaf
richi <richi>
2001-03-20 14:06:25 +0000

    disabled read-mp3 (does not work anyway) - install lame and use read-file instead

--- src/glmid/glame.scm
+++ src/glmid/glame.scm
@@ -1,5 +1,5 @@
 ; glame.scm
-; $Id: glame.scm,v 1.47 2001/03/20 09:55:20 richi Exp $
+; $Id: glame.scm,v 1.48 2001/03/20 14:06:25 richi Exp $
 ;
 ; Copyright (C) 2000 Richard Guenther
 ;
@@ -639,16 +639,17 @@
 
 ;
 ; mp3 reader using the pipe-in filter and mpg123
+; FIXME: does not work due to bug in pipe-in -- disabled
 ;
-(let ((plugin (glame_create_plugin
-		(create-net ((p "pipe-in"))
-	    		()
-	    		((p "out" "out" "output"))
-	    		((p "tail" "filename" "filename"))
-	    		(filternode_set_param p "cmd" "mpg123 -q -s "))
-		"read-mp3")))
-	(plugin_set plugin PLUGIN_DESCRIPTION "mp3-reader")
-	(plugin_set plugin PLUGIN_CATEGORY "InOut"))
+;(let ((plugin (glame_create_plugin
+;		(create-net ((p "pipe-in"))
+;	    		()
+;	    		((p "out" "out" "output"))
+;	    		((p "tail" "filename" "filename"))
+;	    		(filternode_set_param p "cmd" "mpg123 -q -s "))
+;		"read-mp3")))
+;	(plugin_set plugin PLUGIN_DESCRIPTION "mp3-reader")
+;	(plugin_set plugin PLUGIN_CATEGORY "InOut"))
 
 ;
 ; load an external representation of a filter and register it as a plugin
