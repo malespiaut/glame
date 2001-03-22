dc6cf4fbcec0647a8624846f715ab6e1da3eb2f9
xwolf <xwolf>
2001-03-22 00:39:15 +0000

    cancel button works in apply added feed to filter function for feeding data stream into a filter without writing it back to the swapfile
    useful for playing or exporting regions

--- src/gui/waveeditgui.c
+++ src/gui/waveeditgui.c
@@ -197,6 +197,12 @@ static void delete_cb(GtkWidget *bla, GtkWaveView *waveview)
 	gtk_editable_wave_buffer_delete (editable, start, length);
 }
 
+/* shitty workaround for failing gnome_dialog_run_and_close */
+static void setBoolean_cb(GtkWidget *foo, gboolean* bar)
+{
+	*bar = TRUE;
+}
+
 /* GUI is single-threaded, so this should actually work... */
 static GtkWaveView *actual_waveview;
 
@@ -206,13 +212,15 @@ static void apply_cb(GtkWidget *bla, plugin_t *plugin)
 
 	/* FIXME FIXME FIXME 
 	   enable cancel button.
-	   fix richis bogus loop ;) */
+	   fix richis bogus loop ;) 
+	   well. cancel is fine,but that's about it :-( 	*/
 	   
 	GtkWaveView *waveview = actual_waveview;
 	GtkWaveBuffer *wavebuffer = gtk_wave_view_get_buffer (waveview);
 	GtkEditableWaveBuffer *editable = GTK_EDITABLE_WAVE_BUFFER (wavebuffer);
 	GtkSwapfileBuffer *swapfile = GTK_SWAPFILE_BUFFER(editable);
 	GtkWidget *prop;
+	gboolean ok_pressed;
 	gint32 start, length;
 	long *names, nrtracks;
 	filter_t *net, *effect;
@@ -228,7 +236,12 @@ static void apply_cb(GtkWidget *bla, plugin_t *plugin)
 	/* Create one instance of the effect and query the parameters. */
 	effect = filter_instantiate(plugin);
 	prop = glame_gui_filter_properties(filter_paramdb(effect), plugin_name(plugin));
-	fprintf(stderr,"BUTTON: %d\n",gnome_dialog_run_and_close(GNOME_DIALOG(prop)));
+	ok_pressed = FALSE;
+	gtk_signal_connect(GTK_OBJECT(GNOME_PROPERTY_BOX(prop)->ok_button),"clicked",setBoolean_cb,&ok_pressed);
+	gnome_dialog_run_and_close(GNOME_DIALOG(prop));
+	
+	if(!ok_pressed)
+		return;
 
 	/* Create the network, add nrtracks instances of swapfile_in -
 	 * effect - swapfile_out. */
@@ -251,6 +264,88 @@ static void apply_cb(GtkWidget *bla, plugin_t *plugin)
 		filterport_connect(filterportdb_get_port(filter_portdb(eff), PORTNAME_OUT), filterportdb_get_port(filter_portdb(swout), PORTNAME_IN));
 		filterport_connect(filterportdb_get_port(filter_portdb(swin), PORTNAME_OUT), filterportdb_get_port(filter_portdb(eff), PORTNAME_IN));
 	}
+	
+	/* Run the network and cleanup after it. */
+	filter_launch(net);
+	filter_start(net);
+	filter_wait(net);
+	filter_delete(net);
+	filter_delete(effect);
+	gtk_editable_wave_buffer_queue_modified (editable, start, length);
+}
+
+/* kill the running network */
+
+static void stop_network(GtkWidget* bla, plugin_t *net)
+{
+	/* FIXME FIXME FIXME 
+	 * The effect is not cleaned up! */
+	
+	filter_terminate(net);
+	filter_delete(net);
+}
+
+/* Menu event - feed into filter. */
+static void feed_cb(GtkWidget *bla, plugin_t *plugin)
+{
+
+	/* FIXME FIXME FIXME 
+	   fix richis bogus loop ;) */
+	   
+	GtkWaveView *waveview = actual_waveview;
+	GtkWaveBuffer *wavebuffer = gtk_wave_view_get_buffer (waveview);
+	GtkEditableWaveBuffer *editable = GTK_EDITABLE_WAVE_BUFFER (wavebuffer);
+	GtkSwapfileBuffer *swapfile = GTK_SWAPFILE_BUFFER(editable);
+	GtkWidget *prop;
+	gboolean ok_pressed;
+	gint32 start, length;
+	long *names, nrtracks;
+	filter_t *net, *effect;
+	int rate, i;
+	GtkWidget * stop_window;
+
+	gtk_wave_view_get_selection (waveview, &start, &length);
+	if (length <= 0)
+		return;
+	nrtracks = gtk_swapfile_buffer_get_filenames(swapfile, &names);
+	rate = gtk_wave_buffer_get_rate(wavebuffer);
+	DPRINTF("Applying to [%li, +%li]\n", (long)start, (long)length);
+
+	/* Create one instance of the effect and query the parameters. */
+	effect = filter_instantiate(plugin);
+	prop = glame_gui_filter_properties(filter_paramdb(effect), plugin_name(plugin));
+	ok_pressed = FALSE;
+	gtk_signal_connect(GTK_OBJECT(GNOME_PROPERTY_BOX(prop)->ok_button),"clicked",setBoolean_cb,&ok_pressed);
+	gnome_dialog_run_and_close(GNOME_DIALOG(prop));
+	
+	if(!ok_pressed)
+		return;
+	/* Create the network, add nrtracks instances of swapfile_in -
+	 * effect - swapfile_out. */
+	net = filter_creat(NULL);
+
+	
+	
+	for (i=0; i<nrtracks; i++) {
+		filter_t *swin, *eff;
+		int swname = names[i];
+		swin = filter_instantiate(plugin_get("swapfile_in"));
+		eff = filter_creat(effect);
+		filterparam_set(filterparamdb_get_param(filter_paramdb(swin), "filename"), &swname);
+		filterparam_set(filterparamdb_get_param(filter_paramdb(swin), "rate"), &rate);
+		filterparam_set(filterparamdb_get_param(filter_paramdb(swin), "offset"), &start);
+		filterparam_set(filterparamdb_get_param(filter_paramdb(swin), "size"), &length);
+		filter_add_node(net, swin, "swin");
+		filter_add_node(net, eff, "eff");
+		filterport_connect(filterportdb_get_port(filter_portdb(swin), PORTNAME_OUT), filterportdb_get_port(filter_portdb(eff), PORTNAME_IN));
+	}
+	/*   FIXME THIS gets mapped after the network has finished :( useless*/
+	stop_window = gnome_dialog_new("Network running...","Cancel?",NULL);
+	gtk_widget_show(stop_window);
+	gtk_widget_draw_default(stop_window);
+	gnome_dialog_button_connect(GNOME_DIALOG(stop_window),0,stop_network,net);
+	gnome_dialog_set_close(GNOME_DIALOG(stop_window),TRUE);
+
 
 	/* Run the network and cleanup after it. */
 	filter_launch(net);
@@ -262,6 +357,7 @@ static void apply_cb(GtkWidget *bla, plugin_t *plugin)
 }
 
 
+
 static GnomeUIInfo rmb_menu[] = {
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_ITEM("Cut", "cut", cut_cb, NULL),
@@ -271,6 +367,7 @@ static GnomeUIInfo rmb_menu[] = {
 	GNOMEUIINFO_ITEM("Delete", "delete", delete_cb, NULL),
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_SUBTREE("Apply filter", NULL),
+	GNOMEUIINFO_SUBTREE("Feed into filter", NULL),
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_END
 };
@@ -287,6 +384,16 @@ static int choose_effects(plugin_t *plugin)
 	return 1;
 }
 
+/* same as above, but only effects with one input. */
+static int choose_effects_input_only(plugin_t * plugin)
+{
+	filter_t *filter = plugin_query(plugin, PLUGIN_FILTER);
+	if (filterportdb_get_port(filter_portdb(filter), PORTNAME_IN)
+	    && !filterportdb_get_port(filter_portdb(filter), PORTNAME_OUT))
+		return 1;
+	return 0;
+}
+
 /* Button press event. */
 static void press (GtkWidget *widget, GdkEventButton *event,
 		   gpointer user_data) 
@@ -295,6 +402,7 @@ static void press (GtkWidget *widget, GdkEventButton *event,
 	GtkWaveBuffer *wavebuffer;
 	GtkWidget *menu;
 	GtkMenu *filter_menu;
+	GtkMenu *feed_menu;
   
 	if (event->button != 3)
 		return;
@@ -307,8 +415,11 @@ static void press (GtkWidget *widget, GdkEventButton *event,
 
 	menu = gnome_popup_menu_new(rmb_menu);
 	filter_menu = glame_gui_build_plugin_menu(choose_effects, apply_cb);
+	feed_menu = glame_gui_build_plugin_menu(choose_effects_input_only, feed_cb);
 	gtk_widget_show(filter_menu);
+	gtk_widget_show(feed_menu);
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(rmb_menu[7].widget), filter_menu);
+	gtk_menu_item_set_submenu(GTK_MENU_ITEM(rmb_menu[8].widget), feed_menu);
 	actual_waveview = waveview;
 	gnome_popup_menu_do_popup(menu, NULL, NULL, event, waveview);
 }
