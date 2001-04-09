d42f4cc0fbe10d0ea8f3d9cdd9308b2bfe9d2a24
richi <richi>
2001-04-09 09:12:03 +0000

    add sanity check to list_safe_foreach() - PANIC, if used incorrectly

--- src/include/list.h
+++ src/include/list.h
@@ -4,7 +4,7 @@
 /*
  * list.h
  *
- * $Id: list.h,v 1.16 2001/03/28 09:34:57 richi Exp $
+ * $Id: list.h,v 1.17 2001/04/09 09:12:03 richi Exp $
  * 
  * Copyright (C) 1999, 2000 Richard Guenther
  *
@@ -221,9 +221,10 @@ static inline void list_unsplice(struct list_head *list,
 /* Iterate through all list items using entryvar. This is not remove-safe!! */
 #define list_foreach(listp, type, member, entryvar) for (entryvar = list_entry((listp)->next, type, member); &entryvar->member != (listp); entryvar = list_entry(entryvar->member.next, type, member))
 
-/* Iterate through all list items using entryvar. It is safe to remove entryvar, but _only_
- * entryvar. Provide an additional dummy pointer for internal use. */
-#define list_safe_foreach(listptr, type, member, dummy, entryvar) for (entryvar = list_entry((listptr)->next, type, member), dummy = (listptr)->next->next; &entryvar->member != (listptr); entryvar = list_entry((struct list_head *)dummy, type, member), dummy = ((struct list_head *)dummy)->next) 
+/* Iterate through all list items using entryvar. It is safe to remove
+ * entryvar, but _only_ entryvar. Provide an additional dummy pointer
+ * for internal use. */
+#define list_safe_foreach(listptr, type, member, dummy, entryvar) for (entryvar = list_entry((listptr)->next, type, member), dummy = (listptr)->next->next; &entryvar->member != (listptr) && (!list_empty(&entryvar->member) || (DERROR("list_safe_foreach remove error"), 0)); entryvar = list_entry((struct list_head *)dummy, type, member), dummy = ((struct list_head *)dummy)->next) 
 
 /* Count the number of items in the list. */
 static inline int list_count(struct list_head *l)
