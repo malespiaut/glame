5a72934726e4c1138c9cbc316efcd5fed0869efe
richi <richi>
2001-04-18 15:11:44 +0000

    Added Record at marker Removed Feed into and Feed into custom (obsoleted by deletable filters in apply custom)

--- src/gui/waveeditgui.c
+++ src/gui/waveeditgui.c
@@ -438,7 +438,7 @@ static void apply_cb(GtkWidget *bla, plugin_t *plugin)
 	filter_delete(net);
 }
 
-/* Menu event - feed into filter. */
+/* Menu event - play actual selection using audio_out. */
 static void playselection_cb(GtkWidget *bla, plugin_t *plugin)
 {
 	GtkWaveView *waveview = actual_waveview;
@@ -708,111 +708,6 @@ static void recordmarker_cb(GtkWidget *bla, plugin_t *plugin)
 	filter_delete(net);
 }
 
-/* Menu event - feed into filter. */
-static void feed_cb(GtkWidget *bla, plugin_t *plugin)
-{
-	GtkWaveView *waveview = actual_waveview;
-	GtkWaveBuffer *wavebuffer = gtk_wave_view_get_buffer (waveview);
-	GtkEditableWaveBuffer *editable = GTK_EDITABLE_WAVE_BUFFER (wavebuffer);
-	GtkSwapfileBuffer *swapfile = GTK_SWAPFILE_BUFFER(editable);
-	GtkWidget *prop;
-	gboolean ok_pressed;
-	gint32 start, length;
-	long nrtracks;
-	gpsm_swfile_t **files;
-	gpsm_item_t *item;
-	filter_t *net, *effect;
-	int rate, i;
-
-	gtk_wave_view_get_selection (waveview, &start, &length);
-	if (length <= 0)
-		return;
-
-	item = gtk_swapfile_buffer_get_item(swapfile);
-	nrtracks = gtk_swapfile_buffer_get_swfiles(swapfile, &files);
-	rate = gtk_wave_buffer_get_rate(wavebuffer);
-	DPRINTF("Applying to [%li, +%li]\n", (long)start, (long)length);
-
-	/* Create one instance of the effect and query the parameters. */
-	effect = filter_instantiate(plugin);
-	if (filterparamdb_nrparams(filter_paramdb(effect)) > 0) {
-		prop = glame_gui_filter_properties(filter_paramdb(effect), plugin_name(plugin));
-		ok_pressed = FALSE;
-		gtk_signal_connect(GTK_OBJECT(GNOME_PROPERTY_BOX(prop)->ok_button),"clicked",setBoolean_cb,&ok_pressed);
-		gnome_dialog_run_and_close(GNOME_DIALOG(prop));
-		if(!ok_pressed)
-			return;
-	}
-
-	/* Create the network, add nrtracks instances of swapfile_in -
-	 * effect - swapfile_out. */
-	net = filter_creat(NULL);
-
-	for (i=0; i<nrtracks; i++) {
-		filter_t *swin, *eff;
-		swin = create_swapfile_in(files[i], GPSM_ITEM_IS_GRP(item),
-					  start, length);
-		if (!swin)
-			goto fail;
-		filter_add_node(net, swin, "swin");
-		eff = filter_creat(effect);
-		if (!eff)
-			goto fail;
-		filter_add_node(net, eff, "eff");
-		if (!filterport_connect(filterportdb_get_port(filter_portdb(swin), PORTNAME_OUT), filterportdb_get_port(filter_portdb(eff), PORTNAME_IN)))
-			goto fail;
-	}
-
-	glame_gui_play_network(net, NULL, TRUE,
-			       (GtkFunction)network_cleanup_cb,
-			       network_cleanup_create(net, NULL, FALSE));
-	return;
-
- fail:
-	filter_delete(net);
-}
-
-static void feed_custom_cb(GtkWidget * foo, gpointer bar)
-{
-	
-	GtkWaveView *waveview = actual_waveview;
-	GtkWaveBuffer *wavebuffer = gtk_wave_view_get_buffer (waveview);
-	GtkEditableWaveBuffer *editable = GTK_EDITABLE_WAVE_BUFFER (wavebuffer);
-	GtkSwapfileBuffer *swapfile = GTK_SWAPFILE_BUFFER(editable);
-	gint32 start, length;
-	long nrtracks;
-	gpsm_swfile_t **files;
-	gpsm_item_t *item;
-	filter_t *net;
-	int rate, i;
-
-	gtk_wave_view_get_selection (waveview, &start, &length);
-	if (length <= 0)
-		return;
-
-	rate = gtk_wave_buffer_get_rate(wavebuffer);
-	item = gtk_swapfile_buffer_get_item(swapfile);
-	nrtracks = gtk_swapfile_buffer_get_swfiles(swapfile, &files);
-
-	net = filter_creat(NULL);
-	for(i=0;i<nrtracks;i++){
-		filter_t *swin;
-		swin = create_swapfile_in(files[i], GPSM_ITEM_IS_GRP(item),
-					  start, length);
-		if (!swin)
-			goto fail;
-		filter_add_node(net, swin, "swin");
-		filter_set_property(swin,"immutable","1");
-				
-	}
-
-	draw_network(net);
-	return;
-
- fail:
-	filter_delete(net);
-}
-
 static void apply_custom_cb(GtkWidget * foo, gpointer bar)
 {
 	GtkWaveView *waveview = actual_waveview;
@@ -893,21 +788,18 @@ static GnomeUIInfo rmb_menu[] = {
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_SUBTREE("View", view_menu),
 	GNOMEUIINFO_SEPARATOR,
-	GNOMEUIINFO_ITEM("Play", "Plays the whole wave", playall_cb, NULL),
+	GNOMEUIINFO_ITEM("Play all", "Plays the whole wave", playall_cb, NULL),
 	GNOMEUIINFO_ITEM("Play selection", "Plays the actual selection", playselection_cb, NULL),	
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_ITEM("Record at marker", "Records starting at marker position", recordmarker_cb, NULL),	
 	GNOMEUIINFO_ITEM("Record into selection", "Records into the actual selection", recordselection_cb, NULL),	
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_SUBTREE("Apply filter", NULL),
-	GNOMEUIINFO_SUBTREE("Feed into filter", NULL),
-	GNOMEUIINFO_ITEM("Apply Custom", "Creates a filternetwork window for applying it to the selection",apply_custom_cb,NULL),
-	GNOMEUIINFO_ITEM("Feed into Custom", "Creates a filternetworkwindow which and feeds the selection into it", feed_custom_cb,NULL),
+	GNOMEUIINFO_ITEM("Apply custom...", "Creates a filternetwork window for applying it to the selection",apply_custom_cb,NULL),
 	GNOMEUIINFO_SEPARATOR,
 	GNOMEUIINFO_END
 };
 #define RMB_MENU_APPLY_FILTER_INDEX 15
-#define RMB_MENU_FEED_FILTER_INDEX 16
 
 
 /* Somehow only select "effects" (one input, one output) type of
@@ -921,16 +813,6 @@ static int choose_effects(plugin_t *plugin)
 	return 1;
 }
 
-/* same as above, but only effects with one input. */
-static int choose_effects_input_only(plugin_t * plugin)
-{
-	filter_t *filter = plugin_query(plugin, PLUGIN_FILTER);
-	if (filterportdb_get_port(filter_portdb(filter), PORTNAME_IN)
-	    && !filterportdb_get_port(filter_portdb(filter), PORTNAME_OUT))
-		return 1;
-	return 0;
-}
-
 /* Button press event. */
 static void press (GtkWidget *widget, GdkEventButton *event,
 		   gpointer user_data) 
@@ -939,7 +821,6 @@ static void press (GtkWidget *widget, GdkEventButton *event,
 	GtkWaveBuffer *wavebuffer;
 	GtkWidget *menu;
 	GtkMenu *filter_menu;
-	GtkMenu *feed_menu;
   
 	if (event->button != 3)
 		return;
@@ -952,11 +833,8 @@ static void press (GtkWidget *widget, GdkEventButton *event,
 
 	menu = gnome_popup_menu_new(rmb_menu);
 	filter_menu = glame_gui_build_plugin_menu(choose_effects, apply_cb);
-	feed_menu = glame_gui_build_plugin_menu(choose_effects_input_only, feed_cb);
 	gtk_widget_show(GTK_WIDGET(filter_menu));
-	gtk_widget_show(GTK_WIDGET(feed_menu));
 	gtk_menu_item_set_submenu(GTK_MENU_ITEM(rmb_menu[RMB_MENU_APPLY_FILTER_INDEX].widget), GTK_WIDGET(filter_menu));
-	gtk_menu_item_set_submenu(GTK_MENU_ITEM(rmb_menu[RMB_MENU_FEED_FILTER_INDEX].widget), GTK_WIDGET(feed_menu));
 	actual_waveview = waveview;
 	gnome_popup_menu_do_popup(menu, NULL, NULL, event, waveview);
 }
