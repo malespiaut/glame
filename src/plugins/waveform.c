1fd7f8988feb3287a0729758a8af6bd159d60d3a
richi <richi>
2001-04-27 13:27:17 +0000

    Merged the time filter into waveform.c and renamed it to "pluse" - also removed all legacy code.

--- src/plugins/waveform.c
+++ src/plugins/waveform.c
@@ -1,6 +1,6 @@
 /*
  * waveform.c
- * $Id: waveform.c,v 1.16 2001/04/26 11:56:07 richi Exp $
+ * $Id: waveform.c,v 1.17 2001/04/27 13:27:17 richi Exp $
  *
  * Copyright (C) 1999, 2000 Alexander Ehlert
  *
@@ -24,6 +24,7 @@
  * - sine
  * - const
  * - rect
+ * - pulse
  *
  * The waveform filters should only generate "one" buffer which can then
  * be repeated using the repeat filter. [richi]
@@ -40,7 +41,7 @@
 #include "glplugin.h"
 
 
-PLUGIN_SET(waveform, "sine const rect")
+PLUGIN_SET(waveform, "sine const rect pulse")
 
 
 /* Standard waveform connect_out and fixup_param methods. These honour
@@ -303,5 +304,226 @@ int rect_register(plugin_t *p)
 	plugin_set(p, PLUGIN_CATEGORY, "Synthesis");
 	plugin_set(p, PLUGIN_PIXMAP, "rect.png");
 	plugin_set(p, PLUGIN_GUI_HELP_PATH, "Generating_Waves");
+	return filter_register(f, p);
+}
+
+
+typedef enum {
+	PULSE_MODE_NONE=0,
+	PULSE_MODE_ONESHOT=1,
+	PULSE_MODE_INVERT=2
+} pulse_mode_t;
+
+typedef enum {
+	PULSE_STATE_LEVEL,
+	PULSE_STATE_ATTACK,
+	PULSE_STATE_DECAY,
+	PULSE_STATE_DONE
+} pulse_state_t;
+
+static int pluse_f(filter_t *n)
+{
+	filter_pipe_t *out;
+	float rate, dt;
+	float t_on = 0.0, t_off = 0.0;
+	float duration = 0.0, sno = 0.0;
+	float attack = 1.0, release = 1.0;
+	SAMPLE gain;
+	pulse_state_t state = PULSE_STATE_LEVEL;
+	pulse_mode_t mode = PULSE_MODE_NONE;
+	
+	out = filterport_get_pipe(
+		filterportdb_get_port(filter_portdb(n), PORTNAME_OUT));
+	if (!out)
+		FILTER_ERROR_RETURN("no output port");
+
+	rate = filterpipe_sample_rate(out);
+	dt = 1000.0/rate;
+
+	t_on = filterparam_val_float(
+		filterparamdb_get_param(filter_paramdb(n), "pulse_on"));
+	t_off = filterparam_val_float(
+		filterparamdb_get_param(filter_paramdb(n), "t_off"));
+	duration = filterparam_val_float(
+		filterparamdb_get_param(filter_paramdb(n), "duration"));
+
+	attack = filterparam_val_float(
+		filterparamdb_get_param(filter_paramdb(n), "attack"));
+	if (!(attack > 0.0))
+		attack = 1.0;
+	else
+		attack = dt/attack;
+
+	release = filterparam_val_float(
+		filterparamdb_get_param(filter_paramdb(n), "release"));
+	if (!(release > 0.0))
+		release = 1.0;
+	else
+		release = dt/release;
+
+	if (!(t_on > 0.0 && t_off > 0.0))
+		mode |= PULSE_MODE_ONESHOT;
+
+	if (t_off < t_on)
+		mode |= PULSE_MODE_INVERT;
+
+	if (!!(mode & PULSE_MODE_ONESHOT) == !!(~mode & PULSE_MODE_INVERT)) 
+		gain = 1.0;
+	else 
+		gain = 0.0;
+
+	DPRINTF("mode %s%s, gain %f\n", 
+	        mode & PULSE_MODE_ONESHOT ? "oneshot" : "pulse",
+		mode & PULSE_MODE_INVERT ? " invert" : "", 
+		gain);
+
+	FILTER_AFTER_INIT;
+	
+	while (state != PULSE_STATE_DONE) {
+		int i;
+		filter_buffer_t *buf;
+		SAMPLE *s;
+
+		FILTER_CHECK_STOP;
+
+		buf = sbuf_alloc(GLAME_WBUFSIZE, n);
+		if (!buf)
+			break;
+		
+		buf = sbuf_make_private(buf);
+		s = sbuf_buf(buf);
+		
+		for (i=0; i<sbuf_size(buf); sno+=dt, i++) {
+			
+			if (sno > duration) {
+				sbuf_realloc(buf, i);
+				state = PULSE_STATE_DONE;
+				break;
+			}
+			
+			if (sno > t_on)
+				state = PULSE_STATE_ATTACK;
+			
+			if (sno > t_off)
+				state = PULSE_STATE_DECAY;
+			
+			if (state == PULSE_STATE_ATTACK) {
+				gain += attack;
+				if (gain > 1.0) {
+					gain = 1.0;
+					state = PULSE_STATE_LEVEL;
+				}
+			} else if (state == PULSE_STATE_DECAY) {
+				gain -= release;
+				if (gain < 0.0) {
+					gain = 0.0;
+					state = PULSE_STATE_LEVEL;
+				}
+			}
+			*s++ = gain;
+		}
+		sbuf_queue(out, buf);
+	}
+
+	sbuf_queue(out, NULL);
+
+	FILTER_BEFORE_STOPCLEANUP;
+	FILTER_BEFORE_CLEANUP;
+
+	FILTER_RETURN;
+}
+
+static int pluse_connect_out(filter_t *src, filter_port_t *out,
+			     filter_pipe_t *pipe)
+{
+	int rate;
+
+	rate = filterparam_val_int(
+		filterparamdb_get_param(filter_paramdb(src), "rate"));
+	filterpipe_settype_sample(pipe, rate, 0.0);
+
+	return 0;
+}
+
+
+static void pluse_fixup_param(glsig_handler_t *h, long sig, va_list va)
+{
+	filter_t *n;
+	filter_param_t *param;
+	filter_pipe_t *out;
+	int rate;	
+	
+	GLSIGH_GETARGS1(va, param);
+	n = filterparam_filter(param);
+	out = filterport_get_pipe(filterportdb_get_port(
+		filter_portdb(n), "out"));
+	
+	if (!out)
+		return;
+
+	if (strcmp("rate", filterparam_label(param)))
+		return;
+
+	rate = filterparam_val_int(param);
+	
+	if (rate == filterpipe_sample_rate(out))
+		return;
+	
+	filterpipe_settype_sample(out, rate, 0.0);
+	glsig_emit(&out->emitter, GLSIG_PIPE_CHANGED, out);
+}
+
+int pluse_register(plugin_t *p)
+{
+	filter_t *f;
+
+	if (!(f = filter_creat(NULL)))
+		return -1;
+	
+	filterportdb_add_port(filter_portdb(f), PORTNAME_OUT,
+			      FILTER_PORTTYPE_SAMPLE,
+			      FILTER_PORTFLAG_OUTPUT,
+			      FILTERPORT_DESCRIPTION, "output",
+			      FILTERPORT_END);
+
+	filterparamdb_add_param_float(filter_paramdb(f), "time_on",
+			FILTER_PARAMTYPE_TIME_MS, 0.0,
+			FILTERPARAM_DESCRIPTION, "switch on time[ms]",
+			FILTERPARAM_END);
+
+	filterparamdb_add_param_float(filter_paramdb(f), "time_off",
+			FILTER_PARAMTYPE_TIME_MS, 0.0,
+			FILTERPARAM_DESCRIPTION, "switch off time[ms]",
+			FILTERPARAM_END);
+		
+	filterparamdb_add_param_float(filter_paramdb(f), "duration",
+			FILTER_PARAMTYPE_TIME_MS, 0.0,
+			FILTERPARAM_DESCRIPTION, "end output after[ms]",
+			FILTERPARAM_END);
+
+	filterparamdb_add_param_float(filter_paramdb(f), "attack",
+			FILTER_PARAMTYPE_TIME_MS, 0.0,
+			FILTERPARAM_DESCRIPTION, "Attack Time[ms]",
+			FILTERPARAM_END);
+
+	filterparamdb_add_param_float(filter_paramdb(f), "release",
+			FILTER_PARAMTYPE_TIME_MS, 0.0,
+			FILTERPARAM_DESCRIPTION, "Release Time[ms]",
+			FILTERPARAM_END);
+	
+	filterparamdb_add_param_int(filter_paramdb(f), "rate",
+			FILTER_PARAMTYPE_INT, GLAME_DEFAULT_SAMPLERATE,
+			FILTERPARAM_END);
+
+	f->f = pluse_f;
+	f->connect_out = pluse_connect_out;
+	glsig_add_handler(&f->emitter, GLSIG_PARAM_CHANGED, pluse_fixup_param,
+	                  NULL);
+
+	plugin_set(p, PLUGIN_DESCRIPTION, "generates a single ramp or pulse signal");
+	plugin_set(p, PLUGIN_PIXMAP, "pluse.png");
+	plugin_set(p, PLUGIN_CATEGORY, "Synthesis");
+	plugin_set(p, PLUGIN_GUI_HELP_PATH, "Generating_Waves");
+
 	return filter_register(f, p);
 }
