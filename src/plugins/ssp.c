935dc5e4f82dd377f63fd93662d8798b0ea41e78
mag <mag>
2001-05-09 23:02:46 +0000

    moved ssp_streamer back in Analyze category added maxrms plugin, which analyzes the ssp stream and outputs the maximum found rms as filterparam, now the gui can implement a normalizer as 2 pass operation

--- src/plugins/ssp.c
+++ src/plugins/ssp.c
@@ -1,6 +1,6 @@
 /*
  * ssp.c
- * $Id: ssp.c,v 1.1 2001/05/03 17:46:56 mag Exp $
+ * $Id: ssp.c,v 1.2 2001/05/09 23:02:46 mag Exp $
  *
  * Copyright (C) 2001 Alexander Ehlert
  *
@@ -28,14 +28,15 @@
 #include "glplugin.h"
 #include "math.h"
 
-PLUGIN_SET(ssp, "ssp_streamer")
+PLUGIN_SET(ssp, "ssp_streamer maxrms")
 
 static int ssp_streamer_connect_out(filter_t *n, filter_port_t *port,
 				   filter_pipe_t *p)
 {
 	filterpipe_settype_ssp(p, 44100, 64);
 	return 0;
-}          
+}
+
 static int ssp_streamer_f(filter_t *n)
 {
 	filter_pipe_t *in, *out;
@@ -52,9 +53,12 @@ static int ssp_streamer_f(filter_t *n)
 	if (bsize < 1)
 		FILTER_ERROR_RETURN("bsize <= 0");
 	
+	DPRINTF("bsize = %d\n", bsize);
 	fak = 1.0/(SAMPLE)bsize;
 	accu = 0.0;
 	ringbuf = ALLOCN(bsize, SAMPLE);
+	if (ringbuf == NULL)
+		FILTER_ERROR_RETURN("Error allocating ringbuf\n");
 	pos = 0;
 
 	FILTER_AFTER_INIT;
@@ -64,10 +68,10 @@ static int ssp_streamer_f(filter_t *n)
 		FILTER_CHECK_STOP;
 		
 		/* got an input buffer */
-		s = &sbuf_buf(buf)[0];
+		s = sbuf_buf(buf);
 		cnt = sbuf_size(buf);
 		obuf = ssp_make_private(ssp_alloc(cnt, n));
-		ssp = &ssp_buf(obuf)[0];
+		ssp = ssp_buf(obuf);
 		
 		while (cnt--) {
 			ss = *s * *s;
@@ -78,6 +82,7 @@ static int ssp_streamer_f(filter_t *n)
 			s++;
 			*(ssp++) = accu;
 		}
+
 		sbuf_unref(buf);
 		ssp_queue(out, obuf);
 entry:
@@ -115,8 +120,83 @@ int ssp_streamer_register(plugin_t *p)
 	
 	plugin_set(p, PLUGIN_DESCRIPTION, "ssp_streamer");
 	plugin_set(p, PLUGIN_PIXMAP, "ssp.png"); 
-	plugin_set(p, PLUGIN_CATEGORY, "SSP");
-	plugin_set(p, PLUGIN_GUI_HELP_PATH, "SSP");
+	plugin_set(p, PLUGIN_CATEGORY, "Analyze");
+	plugin_set(p, PLUGIN_GUI_HELP_PATH, "Analyze");
+
+	return filter_register(f, p);
+}
+
+static int maxrms_f(filter_t *n)
+{
+	filter_pipe_t	*in;
+	filter_buffer_t	*buf;
+	filter_param_t	*param;
+	
+	SAMPLE	maxrms = 0.0, *s;
+	int	cnt;
+	
+	if (!(in = filternode_get_input(n, PORTNAME_IN)))
+		FILTER_ERROR_RETURN("no input");
+	
+	param = filternode_get_param(n, "maxrms");
+
+	filterparam_set(param, &maxrms);
+
+	DPRINTF("crash vorher?\n");
+	
+	FILTER_AFTER_INIT;
+	
+	goto entry;
+	while (buf) {
+		FILTER_CHECK_STOP;
+		
+		/* got an input buffer */
+		s = sbuf_buf(buf);
+		cnt = sbuf_size(buf);
+		
+		while (cnt--) {
+			if (*s > maxrms)
+				maxrms = *s;
+			s++;
+		}
+
+		sbuf_unref(buf);
+entry:
+		buf = sbuf_make_private(sbuf_get(in));
+	};
+
+	maxrms = sqrtf(maxrms);
+
+	filterparam_set(param, &maxrms);
+
+	FILTER_BEFORE_STOPCLEANUP;
+	FILTER_BEFORE_CLEANUP;
+
+	FILTER_RETURN;
+}
+
+int maxrms_register(plugin_t *p)
+{
+	filter_t *f;
+	filter_paramdb_t *param;
+	
+	if (!(f = filter_creat(NULL)))
+		return -1;
+	
+	filter_add_input(f, PORTNAME_IN, "ssp stream in", FILTER_PORTTYPE_SSP);
+	
+	f->f = maxrms_f;
+	
+	param = filter_paramdb(f);
+
+	filterparamdb_add_param_float(param, "maxrms", FILTER_PARAMTYPE_FLOAT, 0.0 ,
+				    FILTERPARAM_DESCRIPTION, "maximum rms",
+				    FILTERPARAM_END);
+	
+	plugin_set(p, PLUGIN_DESCRIPTION, "maxrms");
+	plugin_set(p, PLUGIN_PIXMAP, "maxrms.png"); 
+	plugin_set(p, PLUGIN_CATEGORY, "Analyze");
+	plugin_set(p, PLUGIN_GUI_HELP_PATH, "Analyze");
 
 	return filter_register(f, p);
 }
