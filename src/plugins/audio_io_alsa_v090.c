5d0e8ca57d0fa04158a2fffe5191d11096afc878
mag <mag>
2001-06-29 22:35:55 +0000

    ok, now cleanup of device configuration with help of PBD's quasimodo code and we've got an alsa in plugin now

--- src/plugins/audio_io_alsa_v090.c
+++ src/plugins/audio_io_alsa_v090.c
@@ -1,6 +1,6 @@
 /*
  * audio_io_alsa_v090.c
- * $Id: audio_io_alsa_v090.c,v 1.6 2001/06/19 10:21:47 mag Exp $
+ * $Id: audio_io_alsa_v090.c,v 1.7 2001/06/29 22:35:55 mag Exp $
  *
  * Copyright (C) 2001 Richard Guenther, Alexander Ehlert, Daniel Kobras
  *
@@ -27,8 +27,279 @@
 #include <string.h>
 #include "audio_io.h"
 
-PLUGIN_SET(audio_io_alsa, "alsa_audio_out")
+PLUGIN_SET(audio_io_alsa, "alsa_audio_in alsa_audio_out")
 
+/*
+configure code shamelessly stolen from Paul Barton Davis       
+*/
+
+int configure_pcm(snd_pcm_t *handle, 
+		  snd_pcm_hw_params_t *hw_params, 
+		  snd_pcm_sw_params_t *sw_params, 
+		  int chancnt,
+		  int rate,
+		  int periodsize)
+{
+  /* just hardcoded fragments = 4 here, because it works with my hoontech card 
+   * FIXME? should we make fragments user configurable
+   */
+
+	int err, format, fragments = 4, psize = 0, dir = 0;
+
+	if ((err = snd_pcm_hw_params_any (handle, hw_params)) < 0)  {
+		DPRINTF("ALSA: no playback configurations available\n");
+		return -1;
+	}
+
+	if ((err = snd_pcm_hw_params_set_periods_integer (handle, hw_params)) < 0) {
+		DPRINTF( "ALSA: cannot restrict period size to integral value.\n");
+		return -1;
+	}
+	/*
+	if ((err = snd_pcm_hw_params_set_access (handle, hw_params, 
+						 SND_PCM_ACCESS_MMAP_NONINTERLEAVED)) < 0) {
+		if ((err = snd_pcm_hw_params_set_access (handle, hw_params, 
+							 SND_PCM_ACCESS_MMAP_INTERLEAVED)) < 0) {
+			DPRINTF("ALSA: mmap-based access is not possible\n");
+			return -1;
+		}
+	}
+	*/
+
+	if ((err = snd_pcm_hw_params_set_access(handle, hw_params,
+						SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+		DPRINTF("ALSA: can't set interleaved rw access\n");
+		return -1;
+	}
+
+#ifdef SND_LITTLE_ENDIAN
+	format = SND_PCM_FORMAT_S16_LE;
+#else
+# ifdef SND_BIG_ENDIAN
+	format = SND_PCM_FORMAT_S16_BE;
+# else
+#error Unsupported endianness.
+# endif
+#endif
+
+	if ((err = snd_pcm_hw_params_set_format (handle, hw_params, format)) < 0) {
+		DPRINTF("Sorry. The audio interface doesn't support signed 16 format");
+		return -1;
+	}
+
+	if ((err = snd_pcm_hw_params_set_rate (handle, hw_params, rate, 0)) < 0) {
+		DPRINTF( "ALSA: cannot set sample/frame rate to %d\n", rate);
+		return -1;
+	}
+
+	/*	nchns = snd_pcm_hw_params_get_channels_max (hw_params);*/
+	
+	DPRINTF("got %d channels to allocate\n", chancnt);
+
+	if ((err = snd_pcm_hw_params_set_channels (handle, hw_params, chancnt)) < 0) {
+		DPRINTF("ALSA: cannot set channel count to %d\n", chancnt);
+		return -1;
+	}
+
+	if ((err = snd_pcm_hw_params_set_periods (handle, hw_params, fragments, 0)) < 0) {
+		DPRINTF("ALSA: cannot set fragment count minimum to %d\n", fragments);
+		return -1;
+	}
+
+	psize = periodsize;
+	if ((err = snd_pcm_hw_params_set_period_size (handle, hw_params, psize, 0)) < 0) {
+		DPRINTF( "ALSA: cannot set fragment length to %d\n", psize);
+		psize = snd_pcm_hw_params_get_period_size(hw_params, &dir);
+		DPRINTF("ALSA: set to %d\n", psize);
+		return -1;
+	}
+
+	if ((err = snd_pcm_hw_params_set_buffer_size (handle, hw_params, fragments * psize)) < 0) {
+		DPRINTF( "ALSA: cannot set buffer length to %d\n", fragments * psize);
+		/*return -1;*/
+	}
+
+	if ((err = snd_pcm_hw_params (handle, hw_params)) < 0) {
+		DPRINTF( "ALSA: cannot set hardware parameters\n");
+		return -1;
+	}
+
+	snd_pcm_sw_params_current (handle, sw_params);
+
+	if ((err = snd_pcm_sw_params_set_start_threshold (handle, sw_params, 0)) < 0) {
+		DPRINTF( "ALSA: cannot set start threshold\n");
+		return -1;
+	}
+
+	if ((err = snd_pcm_sw_params_set_stop_threshold (handle, sw_params, fragments * psize)) < 0) {
+		DPRINTF( "ALSA: cannot set stop threshold\n");
+		return -1;
+	}
+
+	/* dunno what the silence stuff is about ? someone please enlighten me */
+	if ((err = snd_pcm_sw_params_set_silence_threshold (handle, sw_params, 0)) < 0) {
+		DPRINTF( "ALSA: cannot set silence threshold\n");
+		return -1;
+	}
+
+	if ((err = snd_pcm_sw_params_set_silence_size (handle, sw_params, 
+						       0)) < 0) { /* fragments = 0*/
+		DPRINTF("ALSA: cannot set silence size\n");
+		return -1;
+	}
+
+	if ((err = snd_pcm_sw_params_set_avail_min (handle, sw_params, psize)) < 0) {
+		DPRINTF("ALSA: cannot set avail min\n");
+		return -1;
+	}
+
+	if ((err = snd_pcm_sw_params (handle, sw_params)) < 0) {
+		DPRINTF("ALSA: cannot set software parameters\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static int alsa_audio_in_f(filter_t *n)
+{
+	typedef struct {
+		filter_pipe_t	*pipe;
+		filter_buffer_t	*buf;
+		int		pos;
+		int		to_go;
+	} alsa_audioparam_t;
+	
+	alsa_audioparam_t	*in = NULL;
+	gl_s16			*buf = NULL, *is;
+	filter_port_t           *outport;
+	filter_pipe_t		*pipe[2];
+	filter_buffer_t         *obuf;
+	filter_param_t		*param;
+	
+	/* Alsa Stuff */
+
+	int rate = GLAME_DEFAULT_SAMPLERATE;
+	int blksz;
+	long maxsamples = 0, nsamples, dropouts;
+	int endless = 0;
+	int chancnt;	
+	int dir, i, err, j, ret;
+	SAMPLE *s;
+
+	snd_pcm_t		*handle;
+	snd_pcm_hw_params_t	*params;
+        snd_pcm_sw_params_t	*swparams;
+	snd_pcm_status_t 	*status;
+	static snd_output_t 	*log;
+	char*			pcm_name="plughw:0,0";	
+	
+	outport = filterportdb_get_port(filter_portdb(n), PORTNAME_OUT);
+	chancnt = filterport_nrpipes(outport);
+	if (chancnt==0)
+		FILTER_ERROR_RETURN("No output channels!");
+	if (chancnt>2)
+		FILTER_ERROR_RETURN("Only 2 channels supported");
+	
+
+	pipe[0] = filterport_get_pipe(outport);
+	pipe[1] = filterport_next_pipe(outport, pipe[0]);
+
+	if (pipe[1] && filterpipe_sample_hangle(pipe[0]) > 
+		       filterpipe_sample_hangle(pipe[1])) 
+	{ 
+		filter_pipe_t *t = pipe[0]; 
+		pipe[0] = pipe[1]; 
+		pipe[1] = t; 
+	}
+
+	param = filterparamdb_get_param(filter_paramdb(n), "device");
+	if (param)
+		pcm_name = filterparam_val_string(param);
+	
+	param = filterparamdb_get_param(filter_paramdb(n), "rate");
+	if (param)
+		rate = filterparam_val_int(param);
+
+	param = filterparamdb_get_param(filter_paramdb(n), "duration");
+	if (param)
+		maxsamples = (int)(filterparam_val_float(param) * rate);
+	if (maxsamples <= 0.0)
+		endless = 1;
+	
+	/* ALSA specific initialisation.
+	 */
+	
+	if (snd_pcm_open(&handle, pcm_name, SND_PCM_STREAM_CAPTURE, 0))
+		FILTER_ERROR_CLEANUP("Could not open audio device.");
+	
+	/* FIXME: Can we assume s16 native endian is always supported?
+	 *        (The plugin stuff performs a limited set of conversions.)
+	 */
+
+	err = snd_output_stdio_attach(&log, stderr, 0);
+
+	snd_pcm_hw_params_alloca(&params);
+	snd_pcm_sw_params_alloca(&swparams);
+	err = snd_pcm_hw_params_any(handle, params);
+	
+	configure_pcm(handle, params, swparams, chancnt, rate, GLAME_WBUFSIZE);
+
+	snd_pcm_sw_params_dump(swparams, log);
+	snd_pcm_dump(handle, log);
+
+	blksz = snd_pcm_hw_params_get_period_size(params, &dir);
+
+	buf = ALLOCN(blksz*chancnt, gl_s16);
+
+	if (!buf)
+		FILTER_ERROR_CLEANUP("Not enough memory for conversion buffer.");
+
+	snd_pcm_status_alloca(&status);
+	nsamples = 0;
+	dropouts = 0;
+
+	FILTER_AFTER_INIT;
+	
+	while (endless || nsamples < maxsamples) {
+		FILTER_CHECK_STOP;
+
+		if ((ret=snd_pcm_readi(handle, buf, blksz)) != blksz)
+			if ((snd_pcm_status(handle, status))<0) {
+				FILTER_ERROR_STOP("couldn't get device status");
+			}
+		if (snd_pcm_status_get_state(status) == SND_PCM_STATE_XRUN) {
+			dropouts ++;
+			if ((snd_pcm_prepare(handle))<0) {
+				FILTER_ERROR_STOP("read error on alsa device");
+			}
+		}
+		for(i=0;i<chancnt;i++){
+			obuf = sbuf_make_private(sbuf_alloc(blksz, n));
+			s = sbuf_buf(obuf);
+			is = buf + i;
+			for(j=0;j<blksz;j++) {
+				*s++ = SHORT2SAMPLE(*is);
+				is += chancnt;
+			}
+			sbuf_queue(pipe[i], obuf);
+		}
+	}
+	FILTER_BEFORE_CLEANUP;
+	FILTER_BEFORE_STOPCLEANUP;
+
+	for(i=0;i<chancnt;i++)
+		sbuf_queue(pipe[i], NULL);
+
+	DPRINTF("had %ld dropouts while capturing\n", dropouts);
+
+	snd_pcm_close(handle);
+	
+	free(buf);
+	free(in);
+	FILTER_RETURN;
+}
 
 static int alsa_audio_out_f(filter_t *n)
 {
@@ -52,7 +323,7 @@ static int alsa_audio_out_f(filter_t *n)
 	int chunk_size;
 	int to_go, pos;
 	
-	int i, err, buffer_time, period_time, dropouts;
+	int dir, i, err, dropouts;
 	
 	snd_pcm_t		*handle;
 	snd_pcm_format_t	format;
@@ -61,7 +332,7 @@ static int alsa_audio_out_f(filter_t *n)
 	snd_pcm_status_t 	*status;
 	static snd_output_t 	*log;
 	char*			pcm_name="plughw:0,0";	
-	size_t			buffer_size;
+
 	
 	/* Boilerplate init section - will go into a generic function one day.
 	 */
@@ -127,72 +398,17 @@ static int alsa_audio_out_f(filter_t *n)
 
 	snd_pcm_hw_params_alloca(&params);
 	snd_pcm_sw_params_alloca(&swparams);
-	err = snd_pcm_hw_params_any(handle, params);
 	
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("Broken configuration for this pcm");
+	err = configure_pcm(handle, params, swparams, max_ch, rate, GLAME_WBUFSIZE);
 
-	err = snd_pcm_hw_params_set_access(handle, params,
-					   SND_PCM_ACCESS_RW_INTERLEAVED);
+	if (err == -1)
+		FILTER_ERROR_CLEANUP("configuration of audio device failed");
 
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("Access type interleaved not available");
-
-	err = snd_pcm_hw_params_set_format(handle, params, format);
-
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("Unsupported format");
-
-	err = snd_pcm_hw_params_set_channels(handle, params, max_ch);
-
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("unsupported number of channels");
-
-	err = snd_pcm_hw_params_set_rate_near(handle, params, rate, 0);
-
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("unsupported sample rate");
-	
-	/* doesn't do anything, maybe we can tweak latency with it? */
-	err = snd_pcm_hw_params_set_tick_time_near(handle, params, 10000, 0);
-
-	if (err < 0)
-		FILTER_ERROR_CLEANUP("unsupported tick time");
-	
-	/* buffer 400ms, seems a safe setting to me
-	 * again on a fast computer you can probably tune it down
-	 * a fast harddisk definitly helps avoiding dropouts */
-	buffer_time = snd_pcm_hw_params_set_buffer_time_near(handle, params, 
-							400000, 0);
-
-	period_time = buffer_time >> 2;
-	/* default setting for period time is 20ms
-	 * if your computer is fast enough you can lower it */
-	snd_pcm_hw_params_set_period_time_near(handle, params, 20000, 0);
-
-	err = snd_pcm_hw_params(handle, params);
-
-	if (err < 0) {
-		snd_pcm_hw_params_dump(params, log);
-		FILTER_ERROR_CLEANUP("unable to install hardware params");
-	}
-
-	snd_pcm_hw_params_dump(params, log);
-	snd_pcm_sw_params_current(handle, swparams);
-	
-	buffer_size = snd_pcm_hw_params_get_buffer_size(params);
-	blksz = snd_pcm_hw_params_get_period_size(params, 0);
-	err = snd_pcm_sw_params_set_avail_min(handle, swparams, blksz);
-	err = snd_pcm_sw_params_set_start_threshold(handle, swparams, 0);
-	err = snd_pcm_sw_params_set_stop_threshold(handle, swparams, 
-						   buffer_size);
-	
-	if (snd_pcm_sw_params(handle, swparams) < 0) 
-		FILTER_ERROR_RETURN("couldn't install swparams");
-	
 	snd_pcm_sw_params_dump(swparams, log);
 	snd_pcm_dump(handle, log);
 	
+	blksz = snd_pcm_hw_params_get_period_size(params, &dir);
+
 	DPRINTF("Got period size%d\n", blksz);
 
 	out = (gl_s16 *)malloc(blksz*ssize*max_ch);
@@ -305,3 +521,8 @@ int alsa_audio_out_register(plugin_t *p)
 					   alsa_audio_out_f, "plughw:0,0");
 }
 
+int alsa_audio_in_register(plugin_t *p)
+{
+	return aio_generic_register_input(p, "alsa-audio-in",
+					   alsa_audio_in_f, "plughw:0,0");
+}
