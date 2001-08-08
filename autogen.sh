07ec964967b19f7a8a5e57f54bb6e1ab9e4b1e94
nold <nold>
2001-08-08 12:54:04 +0000

    Doing gettextize first thing in autogen.sh seems to be the right way. [dk]

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,10 @@
 
 rm -f config.cache libtool ltconfig ltmain.sh po/Makefile.in.in
 rm -rf intl/
+gettextize --copy >/dev/null
 aclocal -I . -I macros
 libtoolize --copy --automake --ltdl
 autoheader
 autoconf
 touch ltconfig
 automake --copy --add-missing
-gettextize --copy >/dev/null
