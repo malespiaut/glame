dc6ae640baf867e1c5801630db5e533153f47397
nold <nold>
2001-08-08 09:33:47 +0000

    Add gettext framework. [dk]

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 rm -f config.cache libtool ltconfig ltmain.sh
+rm -rf intl/
 aclocal -I . -I macros
 libtoolize --copy --automake --ltdl
 autoheader
 autoconf
 touch ltconfig
 automake --copy --add-missing
+gettextize --copy >/dev/null
