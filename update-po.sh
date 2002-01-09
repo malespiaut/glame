6d75b1025f06eabb3b2ac63be35226e835485f26
richi <richi>
2002-01-09 22:58:56 +0000

    preliminary localization stuff

--- update-po.sh
+++ update-po.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SOURCES="src/gui/main.c src/gui/swapfilegui.c"
+SOURCES="src/gui/main.c src/gui/swapfilegui.c src/gui/apply.c src/gui/glame_accelerator.c src/gui/glame_console.c src/gui/importexport.c src/gui/waveeditgui.c src/gui/edit_filter/canvasfilter.c src/gui/edit_filter/canvaspipe.c src/gui/edit_filter/canvasport.c src/gui/edit_filter/filtereditgui.c src/gui/edit_filter/glamecanvas.c"
 LANGS="de fr"
 
 for l in $LANGS; do
