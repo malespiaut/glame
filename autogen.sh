441c9d7ebf6bf1264e66907244a9b889f0cc0142
richi <richi>
2002-11-08 16:22:25 +0000

    switched to autoconf2.50 and automake1.6 (old ones do no longer work)

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# glame requires at least automake 1.6 and autoconf 2.5
+
 rm -f config.cache libtool ltconfig ltmain.sh po/Makefile.in.in
-rm -rf intl/ ABOUT-NLS libltdl
-yes n | gettextize --copy
-aclocal -I . -I macros
+rm -rf intl/ m4/ ABOUT-NLS libltdl
+autopoint
+aclocal -I . -I macros -I m4
 libtoolize --copy --automake --ltdl
 autoheader
 touch ltconfig
