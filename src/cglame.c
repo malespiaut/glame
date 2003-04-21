bbf639020080ab44d80e0490a2439a9c3732040c
richi <richi>
2003-04-21 16:51:31 +0000

    actually use preferences...

--- src/cglame.c
+++ src/cglame.c
@@ -38,6 +38,118 @@ int cmd_argc;
 char **cmd_argv;
 char *swfname;
 
+
+
+/* Update globals derived from preferences and provide defaults to
+ * all configurables.
+ * Returns -1 if it seems we didnt have a valid config for now. */
+static int update_preferences()
+{
+	filter_t *filter;
+	char *swappath = NULL;
+	char *ainplugin = NULL, *aindev = NULL;
+	char *aoutplugin = NULL, *aoutdev = NULL;
+	char s[256];
+	long maxundo, res = 0;
+	long wbufsize, maxlru, maxfds, maxmaps, maxvm;
+	long rate;
+
+	/* Check, if we have anything configured already. */
+	if (glame_config_get_string("swapfile/defaultpath", &swappath) == -1)
+		res = -1;
+	else
+		free(swappath);
+
+	/* Set default swapfile path and max. undo depth */
+	snprintf(s, 255, "%s/.glameswap", getenv("HOME"));
+	swappath = glame_config_get_string_with_default("swapfile/defaultpath", s);
+
+	glame_config_get_long("swapfile/maxundo", &maxundo);
+	gpsm_set_max_saved_ops(maxundo);
+
+	maxlru = glame_config_get_long_with_default("swapfile/maxlru", 2048);
+	maxfds = glame_config_get_long_with_default("swapfile/maxfds", 128);
+	maxmaps = glame_config_get_long_with_default("swapfile/maxmaps", 256);
+	maxvm = glame_config_get_long_with_default("swapfile/maxvm", 256*1024*1024);
+	swcluster_set_cache(maxlru, maxfds, maxmaps, maxvm);
+
+	/* GLAME_WBUFSIZE */
+	glame_config_get_long("filter/wbufsize", &wbufsize);
+	_GLAME_WBUFSIZE = wbufsize;
+
+	/* Update IO plugin setup - audio_out */
+	aoutplugin = glame_config_get_string_with_default(
+		"audio_io/output_plugin", "audio_out");
+	if (!plugin_get(aoutplugin)) {
+		DPRINTF("No plugin %s - using audio_out\n", aoutplugin);
+		free(aoutplugin);
+		aoutplugin = strdup("audio_out");
+		if (!plugin_get(aoutplugin))
+			goto ain;
+	}
+	aoutdev = filterparam_val_string(filterparamdb_get_param(filter_paramdb((filter_t *)plugin_query(plugin_get(aoutplugin), PLUGIN_FILTER)), "device"));
+	snprintf(s, 255, "%s", aoutdev ? aoutdev : "");
+	aoutdev = glame_config_get_string_with_default("audio_io/output_dev", s);
+	filter = filter_instantiate(plugin_get(aoutplugin));
+	if (filter) {
+		filterparam_set(filterparamdb_get_param(filter_paramdb(filter),
+							"device"), &aoutdev);
+		filter_register(filter, plugin_get("audio_out"));
+	}
+
+ ain:
+	/* Update IO plugin setup - audio_in */
+	rate = glame_config_get_long_with_default(
+		"audio_io/input_rate", GLAME_DEFAULT_SAMPLERATE);
+	ainplugin = glame_config_get_string_with_default(
+		"audio_io/input_plugin", "audio_in");
+	if (!plugin_get(ainplugin)) {
+		DPRINTF("No plugin %s - using audio_in\n", ainplugin);
+		free(ainplugin);
+		ainplugin = strdup("audio_in");
+		if (!plugin_get(ainplugin))
+			goto sync;
+	}
+	aindev = filterparam_val_string(filterparamdb_get_param(filter_paramdb((filter_t *)plugin_query(plugin_get(ainplugin), PLUGIN_FILTER)), "device"));
+	snprintf(s, 255, "%s", aindev ? aindev : "");
+	aindev = glame_config_get_string_with_default("audio_io/input_dev", s);
+	filter = filter_instantiate(plugin_get(ainplugin));
+	if (filter) {
+		filterparam_set(filterparamdb_get_param(filter_paramdb(filter),
+							"device"), &aindev);
+		filterparam_set(filterparamdb_get_param(filter_paramdb(filter),
+							"rate"), &rate);
+		filter_register(filter, plugin_get("audio_in"));
+	}
+
+ sync:
+	/* Sync changes. */
+	glame_config_sync();
+
+	/* Display summary. */
+	DPRINTF(
+"Preferences:\n"
+"\tSwapfile directory %s\n"
+"\tUndo stack depth is %li\n"
+"\tAudio input plugin %s, device \"%s\", rate %li\n"
+"\tAudio output plugin %s, device \"%s\"\n"
+"\tGLAME_WBUFSIZE %i\n",
+                swappath, maxundo, ainplugin, aindev, rate,
+                aoutplugin, aoutdev, _GLAME_WBUFSIZE);
+
+	/* Free temp. storage. */
+	free(swappath);
+	free(aoutplugin);
+	if (aoutdev)
+		free(aoutdev);
+	free(ainplugin);
+	if (aindev)
+		free(aindev);
+
+	return res;
+}
+
+
 /* just enter a scheme command line.
  */
 void sc_main()
@@ -46,6 +158,9 @@ void sc_main()
 		glame_config_get_string("swapfile/defaultpath", &swfname);
 	if (swfname && gpsm_init(swfname) == -1)
 		exit(1);
+
+	/* Update state from preferences. */
+	update_preferences();
 
 	/* Interactive mode. */
 	if (!cmd_argv) {
