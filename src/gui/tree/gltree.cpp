686f75cac724d306be54cce351359ce628e87ea7
richi <richi>
2003-05-25 21:48:02 +0000

    add copyright notices

--- src/gui/tree/gltree.cpp
+++ src/gui/tree/gltree.cpp
@@ -1,6 +1,32 @@
+/*
+ * gltree.cpp
+ *
+ * $Id: gltree.cpp,v 1.2 2003/05/25 21:48:02 richi Exp $
+ *
+ * Copyright (C) 2003 Johannes Hirche, Richard Guenther
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
 
-#include "gltree.h"
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <string.h>
+#include "gltree.h"
 
 
 glTree::glTree(gpsm_grp_t * newroot)
