facde3ae1be6b97e1f7d7eda09ed56d6ae0d2965
richi <richi>
2006-11-25 12:06:55 +0000

    add some guile 1.8 stuff

--- src/glmid/glscript.h
+++ src/glmid/glscript.h
@@ -32,6 +32,11 @@
 #include "gpsm.h"
 #include "swapfile.h"
 
+/* Check if we have guile of at least version 1.8 */
+#undef HAVE_GUILE18
+#if SCM_MAJOR_VERSION == 1 && SCM_MINOR_VERSION >= 8
+#define HAVE_GUILE18 1
+#endif
 
 /* Initializes the guile scripting subsystem. Returns 0 on success,
  * -1 on error. */
